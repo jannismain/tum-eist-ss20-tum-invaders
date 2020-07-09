b9e58c3b7421c08a46a1591b3c55d748c7e92ab5
ge75doc <ge75doc@mytum.de>
2020-07-09 07:05:31 +0300

    Shooting tests implemented

--- test/de/tum/in/ase/eist/ShootingTest.java
+++ test/de/tum/in/ase/eist/ShootingTest.java
@@ -3,6 +3,7 @@ package de.tum.in.ase.eist;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import de.tum.in.ase.eist.view.Bullet;
+import de.tum.in.ase.eist.view.Invader;
 import de.tum.in.ase.eist.view.Player;
 
 public class ShootingTest {
@@ -13,4 +14,21 @@ public class ShootingTest {
         Bullet bullet = player.shoot();
         assertEquals(player.getPosition().getX()+(player.getSize().getWidth()/2)-1, bullet.getPosition().getX());
     }
+    
+    @Test
+    public void testBulletPositionInvader() {
+    	Invader invader = new Invader(20,250);
+        Bullet bullet = invader.shoot();
+        assertEquals(invader.getPosition().getX()+(invader.getSize().getWidth()/2), bullet.getPosition().getX());
+    }
+    
+    @Test
+    public void testInvaderBulletDown() {
+    	Invader invader = new Invader(20,250);
+        Bullet bullet = invader.shoot();
+        assertEquals(invader.getPosition().getY()+(invader.getSize().getHeight()), bullet.getPosition().getY());
+    }
+    
+    
 }
+
