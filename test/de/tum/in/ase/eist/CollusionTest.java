f73f478aaf19fed771d9fef2e388b9a9265970dc
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2020-07-10 18:27:10 +0200

    implement CollisionDetection tests

--- test/de/tum/in/ase/eist/CollusionTest.java
+++ test/de/tum/in/ase/eist/CollusionTest.java
@@ -1,17 +1,25 @@
 package de.tum.in.ase.eist;
+
 import static org.junit.Assert.*;
 
+import de.tum.in.ase.eist.view.Player;
 import de.tum.in.ase.eist.controller.Collision;
 
 public class CollusionTest {
-	
-	public void collisionDetectionTest () {
-		Collision coll= new Collision(null, null);
-		boolean expected = true;
-		boolean actual = coll.detectCollision();
-		assertEquals(expected, actual);
-		
-		
+
+	@Test
+	public void testCollisionDetectionTrue() {
+		Player player = new Player(250, 250);
+		Collision coll = new Collision(player, player);
+		assertTrue(coll.detectCollision());
+	}
+
+	@Test
+	public void testCollisionDetectionFalse() {
+		Player player1 = new Player(250, 250);
+		Player player2 = new Player(400, 400);
+		Collision coll = new Collision(player1, player2);
+		assertFalse(coll.detectCollision());
 	}
 
 }
