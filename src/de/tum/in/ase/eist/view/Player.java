b206b4bd30cf7560b4e047f3a9c1bc38e8502dbc
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2020-07-10 18:32:39 +0200

    add Player(int x, int y) constructor

--- src/de/tum/in/ase/eist/view/Player.java
+++ src/de/tum/in/ase/eist/view/Player.java
@@ -12,12 +12,17 @@ public class Player extends UIElement {
 	 * Constructor for a Player object
 	 */
 	public Player() {
-		super(225, 20);
+		this(225, 20);
+	}
+
+	public Player(int x, int y) {
+		super(x, y);
 		this.setIcon(PLAYER_IMAGE_FILE);
 	}
 
 	public Bullet shoot() {
-		return new Bullet(this.position.getX()+(this.getSize().getWidth()/2)-1, this.position.getY()+this.getSize().getHeight()/2, true);
+		return new Bullet(this.position.getX() + (this.getSize().getWidth() / 2) - 1,
+				this.position.getY() + this.getSize().getHeight() / 2, true);
 	}
 
 }
