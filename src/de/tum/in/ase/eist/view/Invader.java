1dbcf847ab4323701db8ffe5ade6ca46343413be
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2020-07-23 11:27:21 +0200

    remove empty line

--- src/de/tum/in/ase/eist/view/Invader.java
+++ src/de/tum/in/ase/eist/view/Invader.java
@@ -18,6 +18,5 @@ public class Invader extends UIElement {
 	public Bullet shoot() {
 		return new Bullet(this.position.getX() + this.getSize().getWidth() / 2,
 				this.position.getY() - this.getSize().getHeight() / 2, false);
-
 	}
 }
